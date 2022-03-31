166645db213c4634939a858bd625574c569fdfbf
Justin Mirfield <justin.mirfield@gmail.com>
2022-03-31 12:14:18 -0400

    Init commit

---
+++ schema/mutations.js
@@ -0,0 +1,10 @@
+const { GraphQLObjectType } = require("graphql");
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        
+    }
+})
+
+module.exports = mutation
